aabae5f6cf55d5f9c5017f661b64782a3e0e6efb
thibault milan <clawfire@gmail.com>
2011-05-01 23:01:17 +0200

    some seeding datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Plan.create(:title => 'Default', :price => 0, :description => 'Beta testing is so cool that it\'s free')
+Client.create(:display_name => 'Empowebo.com S.A', :firstname => 'Bob' , :lastname => 'Hochmut', :company => 'Empowebo.com S.A', :adress => '13 rue des jardiniers', :zipcode => 'L-1835', :city => 'Luxembourg' , :land => 'Luxembourg', :tel => '+352 26 14 48', :tva => 'SAMPLE', :plan => 1)
